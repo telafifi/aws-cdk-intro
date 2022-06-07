9cb69d75cf771968bfafc0643b2b29df8c9af250
Tarek El-Afifi <tarek@iconbuild.com>
2022-06-07 17:40:12 -0500

    Using Construct Libraries

--- cdk-workshop/lib/hitcounter.ts
+++ cdk-workshop/lib/hitcounter.ts
@@ -12,12 +12,16 @@ export class HitCounter extends Construct {
   /** allows accessing the counter function */
   public readonly handler: lambda.Function;
 
+  /** this hit counter table */
+  public readonly table: dynamodb.Table;
+
   constructor(scope: Construct, id: string, props: HitCounterProps) {
     super(scope, id);
 
     const table = new dynamodb.Table(this, 'Hits', {
       partitionKey: { name: 'path', type: dynamodb.AttributeType.STRING },
     });
+    this.table = table;
 
     this.handler = new lambda.Function(this, 'HitCounterHandler', {
       runtime: lambda.Runtime.NODEJS_14_X,
