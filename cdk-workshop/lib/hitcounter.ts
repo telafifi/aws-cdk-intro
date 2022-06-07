3eb1de3831efc019959a281fcce6a6b50eff2511
Tarek El-Afifi <tarek@iconbuild.com>
2022-06-07 18:05:02 -0500

    Testing Constructs

--- cdk-workshop/lib/hitcounter.ts
+++ cdk-workshop/lib/hitcounter.ts
@@ -6,6 +6,15 @@ import { Construct } from 'constructs';
 export interface HitCounterProps {
   /** the function for which we want to count url hits */
   downstream: lambda.IFunction;
+
+  /**
+   * The read capacity units for the table
+   *
+   * Must be greater than 5 and lower than 20
+   *
+   * @default 5
+   */
+  readCapacity?: number;
 }
 
 export class HitCounter extends Construct {
@@ -16,10 +25,19 @@ export class HitCounter extends Construct {
   public readonly table: dynamodb.Table;
 
   constructor(scope: Construct, id: string, props: HitCounterProps) {
+    if (
+      props.readCapacity &&
+      (props.readCapacity < 5 || props.readCapacity > 20)
+    ) {
+      throw new Error('readCapacity must be greater than 5 and less than 20');
+    }
+
     super(scope, id);
 
     const table = new dynamodb.Table(this, 'Hits', {
       partitionKey: { name: 'path', type: dynamodb.AttributeType.STRING },
+      encryption: dynamodb.TableEncryption.AWS_MANAGED,
+      readCapacity: props.readCapacity ?? 5,
     });
     this.table = table;
 
